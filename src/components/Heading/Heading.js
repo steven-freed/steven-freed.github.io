a463df0b60e29e39e3a8b55c94d5b9995861df4a
Steven Freed <saf236@scarletmail.rutgers.edu>
2020-08-24 15:43:01 -0400

    Updated contact

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -7,8 +7,8 @@ Heading.prototype.markup = function() {
         <img class="profile" src="/assets/me.jpg"></img>\
         <div id="contact">\
             <span>Contact Info:</span>\
-            <span class="contact-info">✉️ freedgitr@gmail.com</span>\
-            <span class="contact-info">💻 steven-freed@github</span>\
+            <span class="contact-info">✉️  <a href="mailto:freedgitr@gmail.com">email</a></span>\
+            <span class="contact-info">💻 <a href="https://github.com/steven-freed">github</a></span>\
             <span class="contact-info">🔗 <a href="https://www.linkedin.com/in/steven-freed-35638713b/">linkedin</a></span>\
         </div>\
         </div>');
